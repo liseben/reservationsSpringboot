1abbff7f01d3acc691c68bb3d1525a322df3cca0
Krefel <elisabethbenga05@gmail.com>
2025-10-22 00:50:08 +0200

    Form delete Artist

--- src/main/java/projet/pid/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/projet/pid/reservationsspringboot/controller/ArtistController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -98,5 +99,16 @@ public class ArtistController {
         
         service.updateArtist(id, artist);
         return "redirect:/artists/" + artist.getId();
+    }
+
+    @DeleteMapping("/artists/{id}")
+    public String delete(@PathVariable int id, Model model) {
+        Artist existing = service.getArtist(id);
+        
+        if (existing != null) {
+            service.deleteArtist(id);
+        }
+        
+        return "redirect:/artists";
     }
 }
